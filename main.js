f2be30c142d59dffb69876abffdbdccfd9ce2732
hanzalajahangir7 <118133960+hanzalajahangir7@users.noreply.github.com>
2024-07-30 17:29:20 +0500

    v1.1

---
+++ main.js
@@ -0,0 +1,8 @@
+// objects in typescript
+var myobj3 = {
+    name: "hanzala",
+    age: 21,
+    car: "ferrari",
+    mode: 2024
+};
+console.log(myobj3);
---
+++ main.ts
@@ -0,0 +1,113 @@
+// objects in typescript
+
+// const myObj = {
+//     name:"hanzala",
+//     age: 20,
+//     rollnum: 2189
+// }
+
+// const myInfo = [myObj.name,myObj.age,myObj.age]
+
+// console.log(myObj.name)
+
+// console.log(myInfo)
+
+// myObj.name = "ali"
+
+// console.log(myObj)
+
+// const myInfo2 = [myObj.name,myObj.age,myObj.rollnum]
+// console.log(myInfo2)
+
+// in 2023
+
+// const student1 = {
+//     name:"hanzala",
+//     age: 20,
+//     rollnum: 2189
+// }
+
+// console.log(`In 2023`,student1)
+// // in 2024
+
+// student1.name = "ali"
+// student1.age = 24
+// student1.rollnum = 2121
+
+
+
+// console.log(`In 2024`,student1)
+
+// Storing a function in object
+
+// const obj1 = {
+//     name: "hanzala",
+//     rollnum:2189,
+//     myfunc:()=>{
+//         return `Hello Mr ${obj1.name} your roll number is ${obj1.rollnum}`
+//     },
+// }
+
+
+// const greetmessage = obj1.myfunc()
+// console.log(greetmessage);
+
+// type alias 
+
+// const myobj2:{
+//     name:string,
+//     age:number
+// } = {
+//     name:"hanzala",
+//     age: 21
+// }
+
+// console.log(myobj2);
+
+// type myType = { 
+//     name:string,
+//     age:number,
+//     footballer:boolean
+// }
+
+// const p1:myType = {
+//     name:"hanzala",
+//     age:21,
+//     footballer: true
+// }
+
+// const p2:myType = {
+//     name:"faizan",
+//     age:13,
+//     footballer: false
+// }
+
+// const p3:myType= {
+//     name:"ahmed",
+//     age:20,
+//     footballer: true
+// }
+
+// console.log(p1,p2,p3);
+
+
+// type A = {
+//     name:string,
+//     age:number
+// }
+
+// type B = {
+//     car:string,
+//     mode:number
+// }
+
+// type combined = A|B
+
+// let myobj3:combined={
+//     name:"hanzala",
+//     age:21,
+//     car:"ferrari",
+//     mode:2024
+// }
+
+// console.log(myobj3);
